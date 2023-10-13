1a6c9146265a266300e27f0eef4e0d9f471a6210
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-10-13 23:19:43 +0200

    Basal backend sat op med app.listen og app.get(/)

--- backend.js
+++ backend.js
@@ -1,11 +1,16 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
+import { request } from "http";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (request, response) => {
+    response.send("https://refactoring.guru/design-patterns/singleton/typescript/example#example-0");
+})
 
 app.listen(port, () => {
   console.log(`Server running on port${port}`);
